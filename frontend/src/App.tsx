45534572bc8d5d592b4cc72538453e5e73760841
Shazinabbas <shazinabbas_m@srmap.edu.in>
2024-11-13 21:58:17 +0530

    Add custom UI components and enhance Landing page layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,19 @@
-import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Landing } from './screens/Landing';
 import { Game } from './screens/Game';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div className='h-screen bg-custom-dark'>
       <BrowserRouter>
       <Routes>
           <Route path="/" element={<Landing />} /> 
           <Route path="/game" element={<Game />} /> 
       </Routes>
     </BrowserRouter>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
