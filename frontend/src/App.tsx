59050b248f7aabe0ab834b802f57fbfcb5396663
Shazinabbas <shazinabbas_m@srmap.edu.in>
2024-11-13 20:18:36 +0530

    Add routing and create Landing and Game components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
-
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Landing } from './screens/Landing';
+import { Game } from './screens/Game';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <button className='bg-red-200'>
-        Join Room
-      </button>
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Landing />} /> 
+          <Route path="/game" element={<Game />} /> 
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
